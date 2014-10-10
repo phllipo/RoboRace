61f84c1424a0f8971a959636024cf9efa305c540
Sascha Maeder <sascha.maeder@otto.de>
2014-10-10 15:04:55 +0200

    Tastatursteuerung implementiert

--- mindstorms/src/main/de/otto/roborace/RacerMain.java
+++ mindstorms/src/main/de/otto/roborace/RacerMain.java
@@ -1,15 +1,53 @@
 package de.otto.roborace;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
 
 public class RacerMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		LCD.clear();
-		LCD.drawString("Gitcrew!", 0, 5);
+		LCD.drawString("TOETEN!", 0, 5);
+		// File soundFile = new File("excellent.wav");
+		// Sound.playSample(soundFile, 1000);
+		Sound.beepSequenceUp();
+		
+        InputStreamReader sr = new InputStreamReader(System.in);
+        char s;
 
-		// Motor.B.rotate(1000 , true);
-		// Motor.C.rotate(1000);
+        while ((s = (char) sr.read()) != -1) {
+
+            if (s == 'w') {
+            	Motor.C.setSpeed(770);
+            	Motor.D.backward();            	
+            }
+            
+            if (s == 'a') {
+            	Sound.beep();
+            	Motor.C.rotate(-20);
+            }
+            
+            if (s == 's') {
+            	Sound.beep();
+            	Motor.D.forward();
+            }
+            
+            if (s == 'd') {
+            	Sound.beep();
+            	Motor.C.rotate(20);
+            }
+            
+            if (s == 'x') {
+            	Motor.D.stop();
+            }
+            
+            
+
+        }	
 
 	}
 //	geradeaus
