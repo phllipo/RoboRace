5aba0c770a6c4f4c26532f21d84343d7308743b4
Sascha Maeder <sascha.maeder@otto.de>
2014-12-05 16:52:15 +0100

    Server verarbeitet nun  Roboauswahl-Messages

--- roboserver/websocket.js
+++ roboserver/websocket.js
@@ -11,15 +11,12 @@ module.exports = {
             id: connectedClients.length
         });
         
-        for (i in connectedClients) {
-            console.log(connectedClients[i]);
-        };
-        
         ws.on('message', function incoming(message) {
             var jsonMessage = JSON.parse(message);
             console.log('received: %s', jsonMessage.eventtype);
             var connectedClient = getClientByWsConnection(ws);
             if (jsonMessage.eventtype === "connect") {
+                connectedClient.name = jsonMessage.data.name;
                 connectedClient.type = jsonMessage.data.clienttype;
                 console.log(connectedClient.type);
             }
@@ -27,7 +24,26 @@ module.exports = {
             	connectedClient.speed = jsonMessage.data.speed;
             	console.log(connectedClient.speed);
             }
+            else if (jsonMessage.eventtype === "selectRobo") {
+                connectedClient.selectedRobo = jsonMessage.data.robo;
+                var controlledRobot = getClientByName(jsonMessage.data.robo);
+                controlledRobot.webSocketConnection.send(JSON.stringify( {
+                  eventtype: "selectedBy",
+                  data: {
+                    playerName: "playerName" }
+                }));                
+            }
+            else if (jsonMessage.eventtype === "ready") {
+              //code  
+            } else {
+              ws.send(JSON.stringify({eventtype: "error", data: { message: "unknownEventtype"}}));
+            }
+          for (i in connectedClients) {
+              console.log(connectedClients[i]);
+          };            
         });
+        
+        
         
         ws.on('close', function(ws) {
             console.log("Client disconnected ...");
@@ -36,6 +52,15 @@ module.exports = {
         function getClientByWsConnection(ws){
             for (i = 0; i <= connectedClients.length; i++) {
                 if (connectedClients[i].webSocketConnection === ws) {
+                    return connectedClients[i];
+                }
+            }
+            return null;
+        }
+        
+        function getClientByName(name) {
+            for (i = 0; i <= connectedClients.length; i++) {
+                if (connectedClients[i].name === name) {
                     return connectedClients[i];
                 }
             }
