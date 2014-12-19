41e9b8af5842b217fdb453d0bdd0d977c659ea6c
b00ters <luca.bernhardt@otto.de>
2014-12-19 15:36:44 +0100

    "yolo" - Patrick, 19.12.2014 verbindung zum webserver mit der app hergestellt!!!!!!

--- RoboApp/app/src/main/java/com/roborace/otto/roboapp/MainActivity.java
+++ RoboApp/app/src/main/java/com/roborace/otto/roboapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.net.URISyntaxException;
+
 
 public class MainActivity extends Activity {
 
@@ -12,28 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        webSocketAppClient client = new webSocketAppClient();
+        try {
+            client.createConnectionToServer();
+        } catch (URISyntaxException e) {
+            System.out.println("Error while connecting: " + e);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
+
 }
