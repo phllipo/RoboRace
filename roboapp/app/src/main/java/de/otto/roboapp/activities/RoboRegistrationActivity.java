786fbcfae39f2d9ec83aa7b660793bee268cd6c5
BoesesNashorn <ariadne.riga@otto.de>
2015-02-20 16:45:23 +0100

    Map Werte werden angezeugt, App-Acitvity RoboRegistration ist in zwei Teile aufgeteilt

--- roboapp/app/src/main/java/de/otto/roboapp/activities/RoboRegistrationActivity.java
+++ roboapp/app/src/main/java/de/otto/roboapp/activities/RoboRegistrationActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.DropBoxManager;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -11,6 +12,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import de.otto.roboapp.R;
 import de.otto.roboapp.RoboAppController;
@@ -27,29 +29,41 @@ public class RoboRegistrationActivity extends Activity {
 
         final RoboAppController roboAppController = (RoboAppController) getApplicationContext();
         final ListView selectRoboList = (ListView) findViewById(R.id.selectRobo_roboList);
+        //TODO
+        // Weitere ListView oder GridView erstellen, um die Map(Elemente) nicht clickable zu machen
+
 
          selectRoboList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 roboAppController.assignPlayerToRobo(selectRoboList.getItemAtPosition(position).toString());
+
 
             }
         });
-
         ArrayAdapter<String> adapter;
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         for (Robo robo : roboAppController.getDataModel().getRoboList()) {
             adapter.add(robo.getName());
         }
-         for (HashMap.Entry<Player, Robo> map :
-                 roboAppController.getDataModel().getPlayerToRoboAssignmentMap().entrySet()) {
-                     adapter.add(map.getKey().getName() + " : " +  map.getValue().getName());
-
-
+        for (HashMap.Entry<Player, Robo> map :
+                roboAppController.getDataModel().getPlayerToRoboAssignmentMap().entrySet()) {
+            adapter.add(map.getKey().getName() + " : " +  map.getValue().getName());
         }
+        /* Schleife um Map Werte von RoboList Werten zu unterscheiden und diese ggf. notClickable zu machen
+        eine andere Alternative ist es eine weitere ListView zu erzeugen und die Map in dieser abzuspeichern.
+        Um diese eigenständig nicht clickable zu machen
+         */
+       /* for(int i = 0; i < adapter.getCount(); i++) {
+            Object obj = adapter.getItem(i);
+
+            for (HashMap.Entry<Player, Robo> map : roboAppController.getDataModel().getPlayerToRoboAssignmentMap().entrySet()) {
+                if (obj == (map.getKey().getName() + " : " +  map.getValue().getName())) {
+                    selectRoboList.setClickable(false);
+                }
+
+            }
+        }*/
         selectRoboList.setAdapter(adapter);
-
-
-    }
-
-}
+}}
--- roboapp/app/src/main/java/de/otto/roboapp/model/DataModel.java
+++ roboapp/app/src/main/java/de/otto/roboapp/model/DataModel.java
@@ -39,6 +39,8 @@ public class DataModel {
 
     }
 
+
+
     public Map<Player, Robo> getPlayerToRoboAssignmentMap() {
         return playerToRoboAssignmentMap;
     }
